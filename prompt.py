4d594249416d1b50ddfd70179124cbba41f95aa4
Tri Pham <tri.pham@hexagon.com>
2026-03-12 14:45:33 -0400

    chore: rename key names to match hf dataset

--- prompt.py
+++ prompt.py
@@ -14,8 +14,8 @@ def main(in_file, out_dir):
         for data in all_datas:
             prompt = prompt_text + '\n\nQuestion: {0}\nA. {1}\nB. {2}\nC. {3}\nD. {4}\nAnswer:'.format(data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -24,10 +24,10 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'correct.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\nA. {2}\nB. {3}\nC. {4}\nD. {5}\nAnswer:'.format(data['correct_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\nA. {2}\nB. {3}\nC. {4}\nD. {5}\nAnswer:'.format(data['default_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -37,8 +37,8 @@ def main(in_file, out_dir):
         for data in all_datas:
             prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\nA. {2}\nB. {3}\nC. {4}\nD. {5}\nAnswer:'.format(data['temporal_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -46,13 +46,13 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'temporal_description.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            if len(data['conflict_time_span']) == 1:
-                prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1} in {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['temporal_conflict_evidence'], data['question'], data['conflict_time_span'][0], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            if len(data['temporal_conflict_time_span']) == 1:
+                prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1} in {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['temporal_conflict_evidence'], data['question'], data['temporal_conflict_time_span'][0], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             else:
-                prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1} from {2} to {3}\nA. {4}\nB. {5}\nC. {6}\nD. {7}\nAnswer:'.format(data['temporal_conflict_evidence'], data['question'], data['conflict_time_span'][0], data['conflict_time_span'][1], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+                prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1} from {2} to {3}\nA. {4}\nB. {5}\nC. {6}\nD. {7}\nAnswer:'.format(data['temporal_conflict_evidence'], data['question'], data['temporal_conflict_time_span'][0], data['temporal_conflict_time_span'][1], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -62,8 +62,8 @@ def main(in_file, out_dir):
         for data in all_datas:
             prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\nA. {2}\nB. {3}\nC. {4}\nD. {5}\nAnswer:'.format(data['semantic_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -73,8 +73,8 @@ def main(in_file, out_dir):
         for data in all_datas:
             prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\n{2} in this quesion means {3}\nA. {4}\nB. {5}\nC. {6}\nD. {7}\nAnswer:'.format(data['semantic_conflict_evidence'], data['question'], data['subject'], data['semantic_description'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -82,10 +82,10 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'misinformation.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\nA. {2}\nB. {3}\nC. {4}\nD. {5}\nAnswer:'.format(data['fact_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            prompt = prompt_text + '\n\nEvidence: {0}\nQuestion: {1}\nA. {2}\nB. {3}\nC. {4}\nD. {5}\nAnswer:'.format(data['misinformation_conflict_evidence_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -93,10 +93,10 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'correct_misinformation.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['correct_evidence'], data['fact_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['default_evidence'], data['misinformation_conflict_evidence_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -104,10 +104,10 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'correct_semantic.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['correct_evidence'], data['semantic_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['default_evidence'], data['semantic_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -115,10 +115,10 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'correct_semantic_description.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\n{3} in this quesion means {4}\nA. {5}\nB. {6}\nC. {7}\nD. {8}\nAnswer:'.format(data['correct_evidence'], data['semantic_conflict_evidence'], data['question'], data['subject'], data['semantic_description'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\n{3} in this quesion means {4}\nA. {5}\nB. {6}\nC. {7}\nD. {8}\nAnswer:'.format(data['default_evidence'], data['semantic_conflict_evidence'], data['question'], data['subject'], data['semantic_description'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -126,10 +126,10 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'correct_temporal.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['correct_evidence'], data['temporal_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2}\nA. {3}\nB. {4}\nC. {5}\nD. {6}\nAnswer:'.format(data['default_evidence'], data['temporal_conflict_evidence'], data['question'], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
 
@@ -137,13 +137,13 @@ def main(in_file, out_dir):
     out_file = out_dir+'/'+'correct_temporal_description.json'
     with open(out_file, 'w', encoding='utf-8') as f:
         for data in all_datas:
-            if len(data['conflict_time_span']) == 1:
-                prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2} in {3}\nA. {4}\nB. {5}\nC. {6}\nD. {7}\nAnswer:'.format(data['correct_evidence'], data['temporal_conflict_evidence'], data['question'], data['conflict_time_span'][0], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+            if len(data['temporal_conflict_time_span']) == 1:
+                prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2} in {3}\nA. {4}\nB. {5}\nC. {6}\nD. {7}\nAnswer:'.format(data['default_evidence'], data['temporal_conflict_evidence'], data['question'], data['temporal_conflict_time_span'][0], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             else:
-                prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2} from {3} to {4}\nA. {5}\nB. {6}\nC. {7}\nD. {8}\nAnswer:'.format(data['correct_evidence'], data['temporal_conflict_evidence'], data['question'], data['conflict_time_span'][0], data['conflict_time_span'][1], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
+                prompt = prompt_text + '\n\nEvidence: {0}\n{1}\nQuestion: {2} from {3} to {4}\nA. {5}\nB. {6}\nC. {7}\nD. {8}\nAnswer:'.format(data['default_evidence'], data['temporal_conflict_evidence'], data['question'], data['temporal_conflict_time_span'][0], data['temporal_conflict_time_span'][1], data['options'][0], data['options'][1], data['options'][2], data['options'][3])
             data = {'prompt': prompt,
-                    'true label': data['correct_option'],
-                    'replaced label': data['replaced_option']}
+                    'true_label': data['correct_option'],
+                    'replaced_label': data['replace_option']}
             json_data = json.dumps(data)
             f.write(json_data+'\n')
             
